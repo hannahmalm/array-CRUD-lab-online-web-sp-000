bd1dbcdc9a09cb936c2cbd883d008c4c902631cf
Hannah Malm <hannahemalm616@gmail.com>
2020-11-01 23:08:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
  
 def create_an_array
-   ["hello", "hi", "hola", "adios"]
+   ["wow","hello", "hi", "hola", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
